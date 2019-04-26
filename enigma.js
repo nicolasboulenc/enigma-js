2f7d167dccb3755662c9207741389f4f570c5a4b
Nicolas Boulenc <nicolasboulenc@gmail.com>
2019-04-26 19:53:54 +0100

    JS Doc

--- enigma.js
+++ enigma.js
@@ -9,13 +9,22 @@
 // Ringstellung = ring offset between inner and outter ring of the rotor
 // Grundstellung = initial position
 
-/** @constant {number} */
+// /** @constant {object} ENIGMA_REFLECTOR_SETTINGS reflector settings. */
+/**
+ * Reflector settings.
+ * @const {Object} ENIGMA_REFLECTOR_SETTINGS
+ * @property {Object} B Type B reflector.
+ * @property {string} B.wiring Wiring.
+ * @property {Object} C Type C reflector.
+ * @property {string} C.wiring Wiring.
+ */
+
 const ENIGMA_REFLECTOR_SETTINGS = {	
 	"B":	{ "wiring": "yruhqsldpxngokmiebfzcwvjat" },
 	"C":	{ "wiring": "fvpjiaoyedrzxwgctkuqsbnmhl" }
 };
 
-/** @constant {number} */
+/** @constant {object} ENIGMA_ROTOR_SETTINGS rotor settings. */
 const ENIGMA_ROTOR_SETTINGS = {	
 	"I":	{ "wiring": "ekmflgdqvzntowyhxuspaibrcj", "notch": "r" },
 	"II":	{ "wiring": "ajdksiruxblhwtmcqgznpyfvoe", "notch": "f" },
@@ -27,7 +36,7 @@ const ENIGMA_ROTOR_SETTINGS = {
 	"VIII":	{ "wiring": "fkqhtlxocbjspdzramewniuygv", "notch": "an" } 
 };
 
-/** @constant {number} */
+/** @constant {object} ENIGMA_DEFAULT_SETTINGS machine default settings for convenience. */
 const ENIGMA_DEFAULT_SETTINGS = {
 	"plugboard":	{ "wiring": "abcdefghijklmnopqrstuvwxyz" },
 	"rotor_right":	{ "type": "III", "offset": "a", "position": "a" },
@@ -37,13 +46,15 @@ const ENIGMA_DEFAULT_SETTINGS = {
 };
 
 
-/** Class representing a point. */
+/** Class representing a rotor. */
 class Rotor {
 
 	/**
-     * Create a point.
-     * @param {number} x - The x value.
-     * @param {number} y - The y value.
+     * Create a rotor.
+     * @param {string} alphabet - The rotor alphabet.
+     * @param {string} type - The type of rotor value, valid values are as defined in ENIGMA_ROTOR_SETTINGS.
+     * @param {string} offset - The inner ring / outer ring offset, refered by the enigma documentation as "ringstellung". Valid values are symbol of alphabet.
+     * @param {string} position - The starting position of a rotor, refered by the enigma documentation as "grundstellung". Valid values are symbol of alphabet.
      */
     constructor(alphabet, type, offset, position) {
 
@@ -58,6 +69,12 @@ class Rotor {
 		this.setup(type, offset, position);
     }
 
+	/**
+     * Change the rotor settings.
+     * @param {string} type - The type of rotor value.
+     * @param {string} offset - The type of rotor value.
+     * @param {string} position - The type of rotor value.
+     */
 	setup(type, offset, position) {
 
 		this.type = type;
@@ -87,6 +104,10 @@ class Rotor {
 		}
 	}
 
+	/**
+     * Rotate the rotor.
+     * @return {boolean} True if the rotor hit a notch, false otherwise.
+     */
 	rotate() {
 
 		this.position++;
@@ -98,6 +119,12 @@ class Rotor {
 		return hit_notch;
 	}
 
+	/**
+     * Feed a letter through the rotor.
+     * @param {string} letter - The type of rotor value.
+     * @param {string} direction - The type of rotor value.
+     * @return {string} The type of rotor value.
+     */
     feed(letter, direction) { 
 
 		// calculate relative position
@@ -121,21 +148,34 @@ class Rotor {
 };
 
 
-// This class is used for the plugboard and the reflector
-// although phisically different their function is identical
+/** Class This class is used for the plugboard and the reflector although phisically different their function is identical. */
 class Mapping {
 
+	/**
+     * Create a plugboard or reflector.
+     * @param {string} alphabet - The plugboard or reflector alphabet.
+     * @param {string} wiring - the mapping of the alphabet to the output.
+     */
 	constructor(alphabet, wiring) {
 
 		this.alphabet = alphabet;
 		this.wiring = wiring;
 	}
 
+	/**
+     * Change the setup of a plugboard or reflector.
+     * @param {string} wiring - the mapping of the alphabet to the output.
+     */
 	setup(wiring) {
 
 		this.wiring = wiring;
 	}
 
+	/**
+     * Feed a letter through the plugboard or reflector.
+     * @param {string} letter - The letter.
+     * @return {string} The output.
+     */
 	feed(letter) { 
 
 		let index = this.alphabet.indexOf(letter);
@@ -144,6 +184,7 @@ class Mapping {
 };
 
 
+/** Class This class is used for the plugboard and the reflector although phisically different their function is identical. */
 class Enigma {
 
 	constructor(alphabet, settings=null, debug=false) {
